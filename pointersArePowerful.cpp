8c6ce7217f69d77ce623c609e642897871cbd580
itachi476 <mderas96@gmail.com>
2025-02-23 23:55:40 -0800

    Add files via upload

--- pointersArePowerful.cpp
+++ pointersArePowerful.cpp
@@ -9,5 +9,13 @@ int main() {
 	std::cout << "The value of integer a, whose address is stored in pointer p is: " << *p << "\n";
 	*p = 443;
 	std::cout << "The new value of integer a, using pointer p is: " << a << "\n";
+	
+	int arr[5] = {21,22,23,25,110};
+	int *arrp = arr;
+	for (int i =0; i <5; i++) {
+		std::cout << "The address of arr[" << i << "] is: " << (arrp + i) << "\n";
+		std::cout << "The value of arr[" << i << "] is: " << *(arrp + i) << "\n";
+	}
+	
 	return 0;
 }
